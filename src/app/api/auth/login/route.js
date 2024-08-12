d0f4363715f9147a3e48857fa002546b2773a304
Michael Debs <michaeldebs16@gmail.com>
2024-08-12 22:31:25 +0300

    Update route.js

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -7,15 +7,18 @@ const createToken = () => {
   return jwt.sign({}, secret);
 };
 
-export async function POST(req) {
+export async function OPTIONS(req) {
   const { password, username } = await req.json();
   res.setHeader('Access-Control-Allow-Origin', 'https://panoramacafe.netlify.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
+
+    return res.status(200).end();
+
+}
+
+export async function POST(req) {
+  const { password, username } = await req.json();
   if (!password) {
     return new Response(JSON.stringify({ error: "Missing Password" }), {
       status: 401,
